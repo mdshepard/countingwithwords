71cf88d6eadbee2db8a29d7e4879d4e5b9dea0e0
Morgan Shepard <morgan@Morgans-MBP.lan>
2018-02-07 16:07:01 -0500

    Yeah, I've gotten 1-20 to appear, as well as every multiple of 10 before 100. That took copious amounts of aid. I'm working on it.

---
+++ main.js
@@ -0,0 +1,13 @@
+// js begin
+
+const lessThan20Array = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen", "twenteh"];
+
+const tensArray = ["twenty", "thirty", "fourty", "fifty", "sixty", "sevety", "eighty", "ninety"]
+
+for (var i = 0; i < lessThan20Array.length; i++) {
+    console.log(lessThan20Array[i])
+}
+
+for (var i = 0; i < tensArray.length; i++) {
+    console.log(tensArray[i]);
+}
